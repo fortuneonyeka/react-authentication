0b6623ef54dcb77510efa9f6b52658e2c4eba7ec
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-08-15 13:35:02 +0100

    setup login and sign up jsx with bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <div className="App">
+      <nav className="navbar navbar-expand navbar-light fixed-top">
+        <div className="container">
+          <a href="" className="navbar-brand">
+            Home
+          </a>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a href="" className="nav-link">
+                  Login
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="" className="nav-link">
+                  Sign Up
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
