ee6c04ebae5b14ef79a3be1ddae1bf5a4f693805
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-08-16 11:00:15 +0100

    added Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/navbar/Navbar";
+import Login from "./components/navbar/Login";
+import Register from "./components/navbar/Register";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 function App() {
   return (
     <div className="App">
-     <Navbar/>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/register" element={<Register/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
